406f1ab03342f4f8abe85f908c52be0c81d546ba
lethosor <lethosor@gmail.com>
2015-04-16 20:13:44 -0400

    Preload libstdc++ on Arch

--- pack/df_linux/distro_fixes.sh
+++ pack/df_linux/distro_fixes.sh
@@ -107,6 +107,11 @@ if [ x"$DF_ARCH" = x'32-bit' ] && [ x"$ARCH" = x'x86_64' ]; then
         find_zlib /lib32/libz.so.1 /lib32
     elif [ x"$OS" = x'Arch' ]; then
         find_zlib /usr/lib32/libz.so /usr/lib32
+        if [ -e "/usr/lib32/libstdc++.so.6" ]; then
+            export PRELOAD_LIB="${PRELOAD_LIB:+$PRELOAD_LIB:}/usr/lib32/libstdc++.so.6"
+        else
+            dlog WARN "Could not find /usr/lib32/libstdc++.so.6"
+        fi
     elif [ x"$OS" = x'Debian' ]; then
         export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}/usr/lib/mesa-diverted/i386-linux-gnu"
         find_zlib /usr/lib32/libz.so /usr/lib32
